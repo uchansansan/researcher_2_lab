984d6dc8add0844f83eed3a362005d8899bcfd32
DogyGamer <33190076+DogyGamer@users.noreply.github.com>
2021-08-23 15:21:36 +0700

    Add files via upload
    Работает балдежно)

--- manual_control.py
+++ manual_control.py
@@ -1,84 +1,105 @@
-#!/usr/bin/env python
-# manual
-
-"""
-This script allows you to manually control the simulator or Duckiebot
-using the keyboard arrows.
-"""
-# from PIL import Image
 import argparse
 import sys
-
 import gym
-import numpy as np
-import cv2 as cv 
 import pyglet
 from pyglet.window import key
-
 from gym_duckietown.envs import DuckietownEnv
-
-# from experiments.utils import save_img
-
 import cv2 as cv
 import numpy as np
 from cv2 import aruco
+import time
+
+
 core = np.ones([3,3])
+
 
 class VideoDetection:
     def __init__(self, visualization=False):
-        self.red_low = np.array([150, 150, 255])
-        self.red_high = np.array([13, 16, 153])
+        self.SQUARE_BOUND = 3000
+        self.DELAY = 200
+        self.RED_LOW = np.array([150, 150, 255])
+        self.RED_HIGH = np.array([13, 16, 153])
         self.vizualization = visualization 
+        self.is_scanned = False
+        self.timer = 0
 
-    def scan_lines(self, img):
-        mask = self.split_mask(self.sdelat_krasivo(cv.inRange(img, self.red_high, self.red_low)))
-        # msk = mask + np.zeros((self.y, self.x))[self.diff_y:, self.diff_x:-self.diff_x]
+
+    def scan(self, img, tick):
+
+        frame_markers = img
+        ids = None
+        lines = self._scan_lines(img)
+
+        if(self.is_scanned and tick-self.timer >= self.DELAY):
+            self.is_scanned = False
+            self.timer = tick
+            return ids, frame_markers
+            
         
+        if(lines is not None and len(lines) >= 2): 
+            corners, ids = self._scan_code(img)
+            square = self._GaussSquare(corners)
+
+            if(square  < self.SQUARE_BOUND):
+                ids = None
+
+            if(self.is_scanned):
+                ids = None
+                
+            if(ids is not None):
+                if(not self.is_scanned):
+                    self.timer = tick
+                    self.is_scanned = True
+                if(self.vizualization):
+                    frame_markers = aruco.drawDetectedMarkers(img.copy(), corners, ids)
+                    cv.putText(frame_markers, str(square), (10,10), cv.FONT_HERSHEY_PLAIN, 1, (0,0,0))
+
+            if (self.vizualization):
+                for line in lines:
+                    x1,y1,x2,y2 = line[0]
+                    cv.line(frame_markers,(x1+self.diff_x,y1+self.diff_y),(x2+self.diff_x,y2+self.diff_y),(0,255,0),2)
+        cv.rectangle(frame_markers, (self.x-self.diff_x, self.y ), (self.diff_x,self.diff_y),(255,0,0), thickness=2)
+        return ids, frame_markers
+
+    def _draw_lines(self, lines):
+        pass
+    def _draw_box(self, corners):
+        pass
+    def _scan_lines(self, img):
+        mask = self._split_mask(self._sdelat_krasivo(cv.inRange(img, self.RED_HIGH, self.RED_LOW)))
         edges = cv.Canny(mask, 50, 150)
         lines = cv.HoughLinesP(edges, 1, np.pi/180, 30, minLineLength=50, maxLineGap=150)
         return lines
 
-
-    def scan_code(self, frame):
+    def _scan_code(self, frame):
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         aruco_dict = aruco.Dictionary_get(aruco.DICT_APRILTAG_36h11)
         parameters =  aruco.DetectorParameters_create()
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
         return corners, ids
 
-    def split_mask(self, mask):
+    def _GaussSquare(self, vertex):
+        right, left, res = 0,0,0
+        if (vertex != []):
+            for i in range(len(vertex[0][0])):
+                if(i+1 < len(vertex[0][0])):
+                    right += vertex[0][0][i][0] * vertex[0][0][i+1][1]
+                    left += vertex[0][0][i][1] * vertex[0][0][i+1][0]
+            res = right - left
+        return abs(res)
+
+    def _split_mask(self, mask):
         y,x = mask.shape
         diff_y, diff_x = round(y/1.4), round(x/7) 
         self.y, self.x, self.diff_x, self.diff_y = y,x, diff_x, diff_y
-
         cropped_mask = mask.copy()[ diff_y:, diff_x:-diff_x]
-        # cv.imshow("mask", cropped_mask)
-        
         return cropped_mask
 
-    def sdelat_krasivo(self, mask):
+    def _sdelat_krasivo(self, mask):
         er = cv.erode(mask, core, iterations=2)
         dil = cv.dilate(er, core, iterations=4)
         return dil
 
-    def scan(self, img):
-        frame_markers = img
-        ids = None
-
-        lines = self.scan_lines(img)
-        if(lines is not None):
-            if(len(lines) >= 2):
-                corners, ids = self.scan_code(img)
-        if (self.vizualization):
-            if(lines is not None):
-                if(len(lines) >=2):
-                    if(ids is not None):
-                        frame_markers = aruco.drawDetectedMarkers(img.copy(), corners, ids)
-                    for line in lines:
-                        x1,y1,x2,y2 = line[0]
-                        cv.line(frame_markers,(x1+self.diff_x,y1+self.diff_y),(x2+self.diff_x,y2+self.diff_y),(0,255,0),2)
-        cv.rectangle(frame_markers, (self.x-self.diff_x, self.y ), (self.diff_x,self.diff_y),(255,0,0), thickness=2)
-        return ids, frame_markers
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env-name", default=None)
@@ -166,7 +187,7 @@ def update(dt):
         delta_v = (v2 - v1) / 2 - wheel_distance / (4 * min_rad) * (v1 + v2)
         v1 += delta_v
         v2 -= delta_v
-
+    # env.unwrapped.step_count
     action[0] = v1
     action[1] = v2
 
@@ -176,18 +197,15 @@ def update(dt):
 
     obs, reward, done, info = env.step(action)
     obs = cv.cvtColor(obs, cv.COLOR_BGR2RGB)
-    ids, frame =  detect.scan(obs)
+
+    ids, frame =  detect.scan(obs, env.unwrapped.step_count)
+
     cv.imshow("game", frame)
+    if(ids is not None):
+        print(ids)
 
 
 
-    print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
-
-    # if key_handler[key.RETURN]:
-
-    #     im = Image.fromarray(obs)
-
-    #     im.save("screen.png")
 
     if done:
         print("done!")
