a1464e86786691e2a639fde8b589b0c4e6d93b44
cat-in-chair <65989148+CatInChair@users.noreply.github.com>
2021-08-23 15:20:05 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from math import sin, cos, pi
 from random import choice
 
-movements = {
-    1: 'l',
-    2: 'f',
-    3: 'r'
-}
 
 class DuckAI:
     # input: data = ((x, y), angle)
@@ -32,8 +27,7 @@ class DuckAI:
         s = movements[0]
         angle_offset = movements[1]
 
-        self.angle += angle_offset
-        self.angle += -360 if self.angle > 180 else (360 if self.angle <= -180 else 0)
+        self.angle += (angle_offset + 180) % 360 - 180
 
         x, y = self.__offset(self.angle, s)
 
@@ -52,31 +46,62 @@ class DuckAI:
                                            and y + self.crossroad['title'] > cross.pos[1],
                              self.points))
         if len(points) > 0:
-            #ji
-            pass
+            print(len(points))
+            point = points[0]
+            if not (point in self.nowRoad.points):
+                self.nowRoad.points.append(point)
+            self.nowCrossRoad = point
+
+            line = None
+
+            lines = list(filter(lambda a: not a.used,point.lines))
+            if len(lines) == 0:
+                next_point = None
+
+                neighbours = self.__neighbour_points(point)
+                for i in neighbours:
+                    if len(list((lambda a: not a.used,i.lines))) > 0:
+                        next_point = i
+                        break
+
+            else:
+                line = choice(list(filter(lambda a: isinstance(a, Road) and not a.used and a != self.nowRoad, point.type)))
+
         else:
             type = data[1][0]
             slicer = (self.angle + data[0]) // 90 + 2
+            now = slicer - (2*int(not slicer % 2))
 
             point = CrossRoad(self, (type, (x, y), slicer))
+            if self.nowRoad:
+                self.nowRoad.points.append(point)
             self.points.append(point)
+
             for j, el in enumerate(point.type):
                 if el == 0:
+                    continue
+                if j == now and self.nowRoad:
+                    point.set_road(self.nowRoad, j)
                     continue
                 road = Road(self, [point], False if j != (slicer+2) % 4 else True)
                 point.set_road(road, j)
                 self.lines.append(road)
+
             line = choice(list(filter(lambda a: isinstance(a, Road) and not a.used, point.type)))
             line.used = True
             move = (point.type.index(line) + slicer - 3) % 4
             self.nowCrossRoad = point
-            if self.nowRoad:
-                self.nowRoad.points.add(point)
             self.nowRoad = line
-            print(move)
 
             # (control callback(move))
-
+    def __neighbour_points(self, point):
+        points = []
+        for i, el in enumerate(point.type):
+            if el == 0:
+                continue
+            elif len(el.points) > 1:
+                points.append(list(filter(lambda a: a!=point,el.points))[0])
+        return points
 
 class CrossRoad:
     # input: duck_bot=DuckAI child, data=(cross type, (x, y), slicer))
@@ -102,10 +127,14 @@ class Road:
         self.points = points
         self.used = used
 
-
+# tests
 duck = DuckAI(((1, 1), 180))
 
 for i in range(5):
     duck.tick((0, 0))
 
-duck.cross_road([1, ['1', '9']])
+duck.cross_road([0, ['1', '8']])
+
+duck.tick((3, 90))
+
+duck.cross_road([0, ['1', '10']])
